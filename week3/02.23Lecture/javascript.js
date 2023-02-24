4d0268346cc459b61bef1f638b42de3587a6d63b
zoe6298 <wwsong0421@gmail.com>
2023-02-23 22:39:42 -0600

    02.23 lecture

--- week3/02.23Lecture/javascript.js
+++ week3/02.23Lecture/javascript.js
@@ -1,14 +1,12 @@
-
-
 function gratuity(billAmount, percentage) {
     return (billAmount * percentage) ;
 }
 console.log(gratuity(100, 0.2));
 
 
-function totalWithGrat(billAmount, percentage) {
+function totalWithGrat(billAmount, gratuityFunction) {
     
-    return (billAmount + gratuity(billAmount, percentage)) ;
+    return (billAmount + gratuityFunction) ;
     
 }
-console.log(`Your total including gratuity is: ${totalWithGrat(100, 0.2)}`)
+console.log(`Your total including gratuity is: ${totalWithGrat(100, gratuity(100,0.2))}`)
