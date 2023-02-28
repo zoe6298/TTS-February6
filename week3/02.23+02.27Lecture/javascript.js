82a585353c6ec8a34ced46aa8d2ca3ef4615346e
zoe6298 <wwsong0421@gmail.com>
2023-02-27 20:53:07 -0600

    03.27

---
+++ week3/02.23+02.27Lecture/javascript.js
@@ -0,0 +1,33 @@
+// function gratuity(billAmount, percentage) {
+//     return (billAmount * percentage) ;
+// }
+// console.log(gratuity(100, 0.2));
+
+
+// function totalWithGrat(billAmount, gratuityFunction) {
+    
+//     return (billAmount + gratuityFunction) ;
+    
+// }
+// console.log(`Your total including gratuity is: ${totalWithGrat(100, gratuity(100,0.2))}`)
+
+// 02.27
+// function outer(){
+//     let a = "hello";
+//     let b = "world";
+  
+//     function inner(a, b) {
+//         console.log(a, b)
+     
+//     }
+  
+//     inner("new","words");
+    
+//   }
+a = ['pop tarts', 'ramen noodles', 'chips', 'salsa','coffee']
+a. push('fruit loops' )
+console.log(a)
+a[4] = 'fair trade coffee'
+
+console.log(a)
+  
