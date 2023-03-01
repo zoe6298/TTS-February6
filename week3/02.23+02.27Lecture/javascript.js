410de1f694a20257a8e9622f70a6e4f418daa15e
zoe6298 <wwsong0421@gmail.com>
2023-03-01 08:23:56 -0600

    02.08 commit

--- week3/02.23+02.27Lecture/javascript.js
+++ week3/02.23+02.27Lecture/javascript.js
@@ -24,10 +24,77 @@
 //     inner("new","words");
     
 //   }
-a = ['pop tarts', 'ramen noodles', 'chips', 'salsa','coffee']
-a. push('fruit loops' )
-console.log(a)
-a[4] = 'fair trade coffee'
+// let a = ['pop tarts', 'ramen noodles', 'chips', 'salsa','coffee']
+// a. push('fruit loops' )
 
-console.log(a)
-  
+// a[4] = 'fair trade coffee'
+// a.splice(2,2, "rice", "beans")
+
+// let shoppingCart = []
+// shoppingCart.push(a.pop())
+// shoppingCart.push(a.shift())
+
+// //8 Write a 'while' loop that takes items from your shopping list and moves them to your cart until there are no items left on the list.
+// while(a.length) {
+//     shoppingCart.push(a.pop())
+// }
+// console.log(a)
+// console.log(shoppingCart)
+// shoppingCart.sort()
+// shoppingCart.reverse()
+// console.log(shoppingCart.toString())
+// 2.28
+let course = {
+	name: 'JavaScript Applications',
+	awesome: true,
+	teachers: ['Assaf', 'Shane'],
+	students: [
+		{
+			name: 'Steve',
+			computer: {
+				OS: 'Linux',
+				type: 'laptop'
+			}
+		},
+		{
+			name: 'Katy',
+			computer: {
+				OS: 'OSX',
+				type: 'macbook'
+			}
+		},
+		{
+			name: 'Chuck',
+			computer: {
+				OS: 'OSX',
+				type: 'macbook'
+			}
+		}
+
+	],
+	preReqs : {
+		skills : ['html', 'css', 'git'],
+		equipment: {
+			laptop: true,
+			OSOptions: ['linux', 'osx']
+		}
+	}
+};
+console.log(course.name)
+console.log(course.teachers[1])
+console.log(course.students[0].name)
+console.log(course.students[1].computer.type)
+console.log(course.preReqs)
+console.log(course.preReqs.equipment.OSOptions[1])
+console.log(course.preReqs.equipment.OSOptions.join(" or "))
+
+let a = []
+for (let i = 0; i < course.students.length; i++) {
+    if (course.students[i].computer.OS === "OSX") {
+        a.push(course.students[i].name);
+        console.log(a)
+        
+        
+    }
+}
+    
