afb658bdfe853e154b6f416ef776de47985965c6
zoe6298 <wwsong0421@gmail.com>
2023-03-08 08:03:07 -0600

    03.07 DOM

--- week4/03.02-lecture-function2/03.06-lecture-DOM/javascript.js
+++ week4/03.02-lecture-function2/03.06-lecture-DOM/javascript.js
@@ -1,14 +1,40 @@
-let header = document.querySelector('header');
-console.log(header);
+// let header = document.querySelector('header');
+// console.log(header);
 
-let sectionItems = document.querySelectorAll("section")
-console.log(sectionItems)
+// let sectionItems = document.querySelectorAll("section")
+// console.log(sectionItems)
 
-let sectionCurrent = document.querySelector("section.current")
-console.log(sectionCurrent)
+// let sectionCurrent = document.querySelector("section.current")
+// console.log(sectionCurrent)
 
-let sectionAfterCurrent = sectionCurrent.nextElementSibling
-console.log(sectionAfterCurrent)
+// let sectionAfterCurrent = sectionCurrent.nextElementSibling
+// console.log(sectionAfterCurrent)
 
-let h2 = sectionCurrent.previousElementSibling.children
-console.log(h2)
+// let h2 = sectionCurrent.previousElementSibling.children
+// console.log(h2)
+// 03.07
+let coffee = document.querySelectorAll('li')[1]
+coffee.innerHTML = "Fair Trade Coffee"
+
+document.querySelectorAll("li")[3].remove()
+let newItem = document.createElement('li')
+newItem.innerHTML = "Cheese Whiz"
+document.querySelector('ul').appendChild(newItem);
+
+a = ['protein powder', 'muscle milk', 'power bars']
+list = document.querySelectorAll('li')
+for (let i = 0; i < list.length; i++) {
+    list[i].remove()
+}
+
+let listItem = [];
+
+for (var i = 0; i < a.length; i++) {
+    listItem[i]= document.createElement("li")
+    listItem[i].innerHTML = a[i]
+    document.querySelector('ul').appendChild(listItem[i])
+}
+
+let muscleMilk = document.querySelectorAll('li')[1]
+muscleMilk.className = ("important")
+console.log(muscleMilk)
