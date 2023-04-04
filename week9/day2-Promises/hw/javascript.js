5513d241901bd8e7b20f9b1db6a0098e3fec5681
zoe6298 <wwsong0421@gmail.com>
2023-04-03 23:19:25 -0500

    async and await hw

--- week9/day2-Promises/hw/javascript.js
+++ week9/day2-Promises/hw/javascript.js
@@ -1,3 +1,23 @@
-function getRandomNumber () {
-    await new Promise
+//async and await
+function getRandomNumber() { 
+    return new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(Math.random());
+    }, 500)
+})
 }
+async function asyncCall() {
+    const result = await getRandomNumber();
+    console.log(result);
+}
+
+asyncCall();
+//fetch
+async function asyncCall2(city) {
+    const result = await fetch(`https://geocode.xyz/${city}?json=1`)
+    const data = await result.json()
+    console.log(data.latt);
+    console.log(data.longt);
+}
+asyncCall2("Seattle");
+asyncCall2("Austin");
